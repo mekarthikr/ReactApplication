f58089929199c8577cf5361aef7ea489400d1c85
Karthik R <iamjustkarthik@gmail.com>
2022-04-25 00:22:16 +0530

    Changes

--- src/index.js
+++ src/index.js
@@ -5,10 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// const arr=['Mikasa','Levi','Eren']
+
+//It id possible to return array of elements
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  
+  <div><h1>Attack on Titan</h1>
+  <p>When man-eating Titans first appeared 100 years ago, humans found safety behind massive walls that stopped the giants in their tracks. But the safety they have had for so long is threatened when a colossal Titan smashes through the barriers, causing a flood of the giants into what had been the humans' safe zone.</p></div>
+  
+
 );
 
 // If you want to start measuring performance in your app, pass a function
